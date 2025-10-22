65dadf8765f3a70b994a4382debee474d10f1cbf
OliverJChristie <olchristie@protonmail.com>
2025-10-22 13:24:50 +1000

    Missing ,

--- recognition/MambaIRv2-48021623/modules.py
+++ recognition/MambaIRv2-48021623/modules.py
@@ -26,7 +26,7 @@ class SimpleColorCNN(nn.Module):
 
             # [B, 32, 96, 96]
             nn.Conv2d(32, 64, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True)
+            nn.ReLU(inplace=True),
 
             # [B, 64, 96, 96]
             nn.Conv2d(64, 32, kernel_size=3, padding=1),
